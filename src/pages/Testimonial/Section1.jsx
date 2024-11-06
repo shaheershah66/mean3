52ce017eee1bcddb499d548ab8c25301502e4c83
shaheershah66 <shaheertabassum@gmail.com>
2024-11-06 18:09:28 +0500

    Updated testimonial page

--- src/pages/Testimonial/Section1.jsx
+++ src/pages/Testimonial/Section1.jsx
@@ -4,7 +4,7 @@ export default function Section1() {
   return (
     <div className="w-full Section1">
       <div className="w-full section1-container">
-        <img className="object-cover w-full h-auto object-center-center" src="/assets/web-banner-clients-testimonial-1-1.jpg" alt="Client Testimonial Background Image"></img>
+        <img className="object-cover w-full h-auto object-center-center" src="assets/web-banner-clients-testimonial-1-1.jpg" alt="Client Testimonial Background Image"></img>
       </div>
     </div>
   );
